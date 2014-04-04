28846a2112134b1eff8c2d3e9ba3dd56bbaebbdf
Scott Christopher Stauffer <scstauf@gmail.com>
2014-04-04 00:00:09 -0500

    triangular number sequence
    This does what digit_count does but better.

---
+++ math.cpp
@@ -0,0 +1,29 @@
+/**
+ * Seven months ago, I thought I had found a previously
+ * unknown integer sequence. But I learned today that
+ * Pythagoras found it first. It's called the triangular
+ * number sequence. I demonstrate another algorithm for
+ * generating the sequence in python. This one is C++
+ * and for more robust. It beats all constraints in the
+ * python algorithm.
+ */
+
+#include <iostream>
+
+int triangular_number(int);
+
+int main() {
+	for (int i = 1; i <= 100; i++) {
+		std::cout << i << ":\t" << triangular_number(i) << std::endl;
+	}
+}
+
+int triangular_number(int base) {
+	int total = 0;
+	
+	for (int i = base; i >= 1; i--) {
+		total += base - i;
+	}
+	
+	return total;
+}
