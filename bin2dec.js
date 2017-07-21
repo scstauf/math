54da9124ef27d90ce050c37fb644c15c531d5a13
Scott Christopher Stauffer <scstauf@gmail.com>
2017-07-20 23:07:11 -0500

    refactored

--- bin2dec.js
+++ bin2dec.js
@@ -3,16 +3,17 @@
 function bin2dec(base2) {
   var base10 = 0,
       n = 0,
-      i = 0;
+      i = 0,
+      len = (base2 && typeof base2 === 'string') ? base2.length : 0;
 
-  for (i = 0; i < base2.length; i++) {
+  for (i = 0; i < len; i++) {
     n = parseInt(base2[i]);
 
     if (isNaN(n) || n > 1) {
       throw new Error('did not supply a valid base2 string');
     }
 
-    base10 += ((1 << (base2.length - i - 1)) * n);
+    base10 += ((1 << (len - i - 1)) * n);
   }
 
   return base10;
