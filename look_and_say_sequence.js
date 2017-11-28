e2f27fc607ec3d09c11931bdebf4cc61155e2af1
Scott Christopher Stauffer <scstauf@gmail.com>
2017-11-28 16:44:37 -0600

    Update look_and_say_sequence.js

--- look_and_say_sequence.js
+++ look_and_say_sequence.js
@@ -6,30 +6,34 @@
  */
 
 function lookAndSay(ns) {
-	var i = 0, count = 0,
-		c = '\0',
-		last = '\0',
-		seq = '';
-	
-	if (typeof ns !== 'string' || ns.length === 0) return ns;
-	
-	for (var i = 0, c = ns[0]; i < ns.length; i++, c = ns[i]) {
-		if (i === 0) {
-			last = c;
-		}
-		else if (last !== c) {
-			seq += count + last;
-			last = c;
-			count = 0;
-		}
-		
-		++count;
-	}
-	
-	if (last !== '\0') {
-		seq += count + last;
-	}
-	
-	console.log(seq);
-	return seq;
+    var i = 0, count = 0,
+        c = '\0',
+        last = '\0',
+        seq = '';
+    
+    if (typeof ns !== 'string' || ns.length === 0) return ns;
+    
+    for (var i = 0, c = ns[0]; i < ns.length; i++, c = ns[i]) {
+        if (i === 0) {
+            last = c;
+        }
+        else if (last !== c) {
+            seq += count + last;
+            last = c;
+            count = 0;
+        }
+        
+        ++count;
+    }
+    
+    if (last !== '\0') {
+        seq += count + last;
+    }
+    
+    console.log(seq);
+    return seq;
+}
+
+for (var i = 0, s = '1'; i < 50; i++) {
+    s = lookAndSay(s);
 }
